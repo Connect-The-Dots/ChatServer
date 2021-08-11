f3b1f83e1fbd2835fdda9c22ddc11ed6357c98d6
tgloverix <tgloverxi0819@sina.com>
2021-08-11 15:49:00 +0800

    fix bugs

--- chat_server/server.js
+++ chat_server/server.js
@@ -39,7 +39,7 @@ const server = https.createServer(app);
 const io = socket(server);
 
 
-const PORT = 10000 || process.env.PORT;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => console.log(`>> server is listening to port: ${PORT}`));
 
